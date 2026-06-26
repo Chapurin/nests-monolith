070f7bdab8c72ec33d21c4c99fd08beb3ffc1280
Chapurin <chapurin.vladimir.v@yandex.ru>
2026-06-26 20:57:04 +0400

    feat(elastic): inject PrismaService into ElasticService
    Also add the typecheck npm script.

--- src/elastic/elastic.service.ts
+++ src/elastic/elastic.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'nestjs-prisma';
 
 @Injectable()
-export class ElasticService {}
+export class ElasticService {
+  constructor(
+    private readonly elasticsearchService: ElasticService,
+    private readonly prismaService: PrismaService,
+  ) {}
+}
