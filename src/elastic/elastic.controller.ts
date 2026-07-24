38c2260a4b77b72b22ff90ccef4caf150f4bb036
Chapurin <chapurin.vladimir.v@yandex.ru>
2026-07-24 17:12:38 +0400

    feat(elastic): add index-all-products, indices and search endpoints

--- src/elastic/elastic.controller.ts
+++ src/elastic/elastic.controller.ts
@@ -1,7 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Query } from '@nestjs/common';
 import { ElasticService } from './elastic.service';
 
 @Controller('elastic')
 export class ElasticController {
   constructor(private readonly elasticService: ElasticService) {}
+
+  @Post('index-all-products')
+  async indexAllProductsWithBrands() {
+    await this.elasticService.deleteIndexProductsWithBrand();
+    await this.elasticService.createIndexProductsWithBrands();
+    await this.elasticService.indexAllProductsWithBrand();
+  }
+
+  @Get('indices')
+  async getIndices() {
+    return this.elasticService.getIndices();
+  }
+
+  @Get('search')
+  async search(@Query('q') query: string) {
+    return this.elasticService.searchProductsWithBrands(query);
+  }
 }
